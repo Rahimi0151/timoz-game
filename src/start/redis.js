8d70d4034a18573bb85593eb1bad841e78950aba
rahimi0151 <rahimi0151@gmail.com>
2023-08-09 23:44:20 +0330

    FIX added ioredis instead of redis

--- src/start/redis.js
+++ src/start/redis.js
@@ -1,25 +1,23 @@
 const config = require('config')
-const redis = require('redis');
+const Redis = require('ioredis');
 
 class RedisSingleton {
     constructor() {
-        if (!RedisSingleton.instance) {
-            this.client = redis.createClient({
-                host: config.get('redis.host'),
-                port: config.get('redis.port'),
-                db: config.get('redis.db')
-            });
-            RedisSingleton.instance = this;
-        }
-        this.client.connect()
+        if (RedisSingleton.instance) return RedisSingleton.instance;
+
+        this.client = new Redis({
+            host: config.get('redis.host'),
+            port: config.get('redis.port'),
+            db: config.get('redis.db')
+        });
+        RedisSingleton.instance = this;
+
         return RedisSingleton.instance;
     }
-    
+
     getClient() {
         return this.client;
     }
 }
 
-const redisSingleton = new RedisSingleton();
-
-module.exports = redisSingleton;
+module.exports = new RedisSingleton();
